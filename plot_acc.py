208e953ab84963e56d3a2e2a78531bcff201d36c
jxu <7989982+jxu@users.noreply.github.com>
2020-12-15 07:28:29 +0000

    Add loss plotting code

---
+++ plot_acc.py
@@ -0,0 +1,14 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy
+
+history = pickle.load(open("baseline.pkl", "rb"))
+print(history.history.keys())
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
